32597736626d84a9a3bdb950c08854abd0c10c11
Vani-Nigam07 <108229479+Vani-Nigam07@users.noreply.github.com>
2025-12-26 21:56:40 +0000

    Deploying to gh-pages from @ Vani-Nigam07/Portfolio@4db17c8deba210e4a4bf0287674c8b5acf09aba2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -479,14 +479,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/Portfolio/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/Portfolio/assets/pdf/Vani_Nigam_MS_2025.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%76%6E%69%67%61%6D@%61%6E%64%72%65%77.%63%6D%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -508,13 +508,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
